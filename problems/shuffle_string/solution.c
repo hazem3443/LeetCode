8c5943ccbd44daa286aa57c4c65417ab23938205
hazem3443 <hazemkhaled3443@gmail.com>
2023-04-21 03:19:49 +0200

    deleted old files and modify .yml file

--- problems/shuffle_string/solution.c
+++
@@ -1,9 +0,0 @@
-
-
-char * restoreString(char * s, int* indices, int indicesSize){
-    char *new_s = calloc(indicesSize+1, sizeof(char));
-    for (int i = 0; i < indicesSize; i++)
-        new_s[indices[i]] = s[i];
-
-    return new_s;
-}

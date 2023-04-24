59cc09ac0ce2066a49a06bc2954803176d9c106b
Hazem Khaled <hazemkhaled3443@gmail.com>
2023-04-24 18:06:44 +0200

    Time: 5 ms (41.86%) | Memory: 9.2 MB (54.70%) - LeetSync

--- longest-common-prefix/longest-common-prefix.cpp
+++ longest-common-prefix/longest-common-prefix.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int maxmatchidx_of_each_str_in_vect=1000;
+        
+        int maxmatchidx_of_each_str_in_vect = std::numeric_limits<int>::max();
 
         while (strs.size() != 1)
         {
-        std::vector<std::string>::iterator it = strs.begin();
+            int i=0;
+            while( (strs[0][i] == strs[1][i]) && i++ < (strs[0].size()>strs[1].size()?strs[0].size():strs[1].size()) );
 
-        std::string s1=strs[0], s2=strs[1];
-        int i=0;
-        while( (s1[i] == s2[i]) && i++ < (s1.size()>s2.size()?s1.size():s2.size()) );
-
-        maxmatchidx_of_each_str_in_vect =\
-                i<maxmatchidx_of_each_str_in_vect?i:maxmatchidx_of_each_str_in_vect;
-   
+            maxmatchidx_of_each_str_in_vect =\
+                    i<maxmatchidx_of_each_str_in_vect?i:maxmatchidx_of_each_str_in_vect;
+    
             strs.erase(strs.begin());
         }
-        
+
         return strs[0].substr(0, maxmatchidx_of_each_str_in_vect);
     }
 };
